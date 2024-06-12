d90f9f97640e083ca79d14790f60ab2fd4e98a40
hyojoongkim17 <high0301@gmail.com>
2024-06-12 22:03:12 +0900

    로그 과거-현재 배경변경 스크롤트리거 작업

--- private/cocofo_log/DX3_G/trueOrFiction/common.js
+++ private/cocofo_log/DX3_G/trueOrFiction/common.js
@@ -1,7 +1,56 @@
 window.onload = function () {
+    gsap.registerPlugin(ScrollTrigger);
+
     const allChat = document.querySelectorAll('.chat')
     allChat.forEach((value, key) => {
         value.addEventListener('click', chatClickEvnet);
+    })
+
+    window.addEventListener("scroll", scrollEvent);
+
+    for (let i= 1; i <= logData.scene; i++) {
+        // ScrollTrigger.create({
+        //     trigger: "#scene"+i,
+        //     start: "top top",
+        //     endTrigger: "#scene"+(i+1),
+        //     end: "bottom bottom",
+        //     onToggle: self => console.log("start"),
+        //     onUpdate: self => {
+        //         document.querySelector("#container").style.background = `rgba(${i*30},${i*30},${i*30},1)`;
+        //     }
+        // })
+    }
+    ScrollTrigger.create({
+        trigger: ".past-scene-start",
+        start: "0 50%",
+        endTrigger: ".past-scene-end",
+        end: "100% 100%",
+        onUpdate: self => {
+            document.querySelector("#container").style.background = '#000';
+        }
+    })
+    ScrollTrigger.create({
+        trigger: ".past-scene-end",
+        start: "0 50%",
+        endTrigger: ".past-scene-start-2",
+        end: "100% 100%",
+        onUpdate: self => {
+            document.querySelector("#container").style.background = '#fff';
+        }
+    })
+    ScrollTrigger.create({
+        trigger: ".past-scene-start-2",
+        start: "0 50%",
+        onUpdate: self => {
+            document.querySelector("#container").style.background = '#000';
+        }
+    })
+    ScrollTrigger.create({
+        trigger: ".past-scene-end-2",
+        start: "0 50%",
+        onUpdate: self => {
+            document.querySelector("#container").style.background = '#fff';
+        }
     })
 }
 
@@ -22,7 +71,7 @@ function getScenePosition(scene) {
 }
 
 const logData = {
-    scene: Array(20),
+    scene: 20,
     player: {
         miho: new PlayerData('miho','a', '#ff9800'),
         nagai: new PlayerData('nagai','a', '#f44336'),
@@ -35,8 +84,7 @@ const logData = {
     ]
 }
 
-console.log(logData.player.miho);
-function scrollEvent() {
+const scrollEvent = (e) => {
 
 }
 

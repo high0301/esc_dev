6c495ce15232307f61c652e2af88a5e0649b9752
hyojoongkim17 <hyojoong17@gamail.com>
2024-06-12 17:49:18 +0900

    스크립트 작업 시작

---
+++ private/cocofo_log/DX3_G/trueOrFiction/common.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+    const allChat = document.querySelectorAll('.chat')
+    allChat.forEach((value, key) => {
+        value.addEventListener('click', chatClickEvnet);
+    })
+}
+
+
+function PlayerData(name, image, color) {
+    this.name = name;
+    this.image = image;
+    this.color = color;
+}
+
+function chatData(name, chat) {
+    this.name = name;
+    this.chat = chat;
+}
+const logData = {
+    scene: Array(13),
+    player: {
+        miho: new PlayerData('miho','a', '#ff9800'),
+        nagai: new PlayerData('nagai','a', '#f44336'),
+        miyu: new PlayerData('miyu','a', '#ffaec9'),
+        ama: new PlayerData('ama','a', '#e0e0e0'),
+        mai: new PlayerData('mai','a', '#ffeb3b'),
+    },
+    background: [
+        ''
+    ]
+}
+
+console.log(logData.player.miho);
+function scrollEvent() {
+
+}
+
+const chatClickEvnet = (e) => {
+    console.log(e.target);
+}
